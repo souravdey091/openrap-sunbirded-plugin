4a7ce925b166291ef736e4f149460073b9dde36f
Sourav Dey <sourav_d@techjoomla.com>
2020-03-23 12:05:17 +0530

    Issue #SB-18384 feat: Use of device registered token in desktop app

--- server/server.ts
+++ server/server.ts
@@ -103,7 +103,7 @@ export class Server extends BaseServer {
     }
     const pluginConfig = {
       pluginVer: manifest.version,
-      apiToken: await containerAPI.getDeviceSdkInstance().getToken(),
+      apiToken: "",
       apiBaseURL: process.env.APP_BASE_URL,
       apiTokenRefreshFn: "refreshToken"
     };
